80398b255ab3ff8836349c7ee5a954a3b98ae8b9
jhaysonsa <sonahj00@gmail.com>
2017-04-13 00:29:01 +0800

    First commit

---
+++ MvcAuction/Models/Auction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace MvcAuction.Models
+{
+    public class Auction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime StarTime   { get; set; }
+
+
+    }
+}
